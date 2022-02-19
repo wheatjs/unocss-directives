d368a8940de1d2eee6f2ccf9460f412372aad121
wheatjs <jacobrclevenger@gmail.com>
2022-02-19 01:16:23 -0500

    fix: update

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts', 'src/vite.ts'],
   splitting: false,
-  sourcemap: true,
   dts: true,
   format: ['cjs', 'esm'],
   external: [
